afce92981951f8116f1890f5ead300929dc9bc21
bilal <bilal@ubuntu.ubuntu-domain>
2022-07-23 16:38:09 +0500

    add more info

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -227,7 +227,7 @@ class SignupForm extends Component {
 
                         this.setState(
                             {
-                                error: "Account is Created ✅ Master Password: " + accountInfo.masterPassword + ' | Posting Key' + accountInfo.posting, 
+                                error: "Account is Created ✅ Master Password: " + accountInfo.masterPassword + ' Posting Key: ' + accountInfo.posting, 
                                 availbtn: false,
                                 genbtn: false,
                                 inputfield: true
